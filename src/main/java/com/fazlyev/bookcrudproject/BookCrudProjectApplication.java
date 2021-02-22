c5b2daecb5b090dc0638cce940ea1c4848861e26
Azat Fazlyev <fazliev14@gmail.com>
2021-02-22 17:08:49 +0300

    Initial commit

---
+++ src/main/java/com/fazlyev/bookcrudproject/BookCrudProjectApplication.java
@@ -0,0 +1,13 @@
+package com.fazlyev.bookcrudproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookCrudProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookCrudProjectApplication.class, args);
+    }
+
+}
